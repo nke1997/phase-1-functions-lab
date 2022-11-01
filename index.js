4a5b7fd4c6f260b91c39734902da3bcbaa6c03b1
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-01 11:10:09 -0400

    commit tree 26dc4fc1bbdee6b80623fbdcdc63ec4c732e38b1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(42 - street);
+};
+
+function distanceFromHqInFeet(street) {
+    return (distanceFromHqInBlocks(street) * 264); 
+}
